d20f7656a6c70a060f577748d73645500de25437
Nurzat Amirzhan <zhanna910411@gmail.com>
2024-10-30 22:24:59 +0300

    ноутбук жайлы костым

---
+++ index.js
@@ -0,0 +1,13 @@
+let makName = "DESKTOP-AC9B6H9"; //ноутбук аты
+let processor = "Intel(R) Core(TM) i7-10750H CPU @ 2.60GHz   2.59 GHz" //процессор атs
+let size = "Білмим таппай койдым" //экран размері
+let color = "Black" //ноутбук түсі
+let todayWather = "+7-+10 °C таза аспан бұлтсыз"//Қазіргі ауа райы
+let currenTime = "22:22"//қазіргі уақыт
+
+console.log("Menym noutbugymnyn aty", makName)
+console.log("Menym noutbugymnyn processory", processor)
+console.log("Menym noutbugymnyn razmery", size)
+console.log("Menym noutbugymnyn tusy", color)
+console.log("Қазіргі ауа райы", todayWather)
+console.log("қазіргі уақыт", currenTime)
